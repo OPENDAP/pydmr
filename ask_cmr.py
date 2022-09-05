dc8bd296428a9718a70ef84398df60c2c1ee4481
James Gallagher <jgallagher@opendap.org>
2022-09-04 20:16:36 -0600

    Reorganization

--- ask_cmr.py
+++ ask_cmr.py
@@ -23,6 +23,7 @@ def main():
     group = parser.add_mutually_exclusive_group() # only one option in 'group' is allowed at a time
     group.add_argument("-p", "--provider", help="a provider id, by itself, print all the providers collections")
     group.add_argument("-c", "--collection", help="a collection id, by itself, print some info")
+    group.add_argument("-r", "--resty-path", help="get the data URL for an OPeNDAP EDC REST URL")
 
     parser.add_argument("-o", "--opendap", help="for a provider, show only collections with opendap URLS", action="store_true")
     parser.add_argument("-g", "--granules", help="for a collection, get info about all the granules", action="store_true")
@@ -40,6 +41,8 @@ def main():
             entries = cmr.get_collection_granules(args.collection, pretty)
         elif args.collection:
             entries = cmr.get_collection_entry(args.collection, pretty)
+        elif args.resty_path:
+            entries = cmr.decompose_resty_url(args.resty_path, pretty)
         else:
             entries = cmr.get_provider_collections(args.provider, opendap, pretty)
         duration = time.time() - start
