c72ab915303b5dfd9a4e56dfb878726634a3e657
James Gallagher <jgallagher@opendap.org>
2022-11-27 10:31:41 -0700

    Started down the subprocess path for the tests

--- run_tests.py
+++ run_tests.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 """
-Find all the NASA ESDIS Providers in the CMR catalog that have collections
-accessible using OPeNDAP.
+Run a series of tests on a group of providers.
 """
 
-import xml.dom.minidom as minidom
+import subprocess
 import time
 
 import cmr
@@ -13,8 +12,7 @@ import cmr
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(description="Query CMR and get information about Providers with Collections "
-                                                 "accessible using OPeNDAP.")
+    parser = argparse.ArgumentParser(description="Run a series of tests on a group of providers.")
 
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
     parser.add_argument("-P", "--pretty", help="request pretty responses from CMR", action="store_true", default=False)
@@ -23,23 +21,11 @@ def main():
     parser.add_argument("-x", "--xml", help="time responses from CMR", action=argparse.BooleanOptionalAction, default=True)
     parser.add_argument("-V", "--version", help="version number for test results XML file", action="store_true", default="1")
 
-    group = parser.add_mutually_exclusive_group(required=True)  # only one option in 'group' is allowed at a time
-    group.add_argument("-e", "--environment", help="an environment, a placeholder for now. This only works for PROD.")
-
     args = parser.parse_args()
 
     cmr.verbose = True if args.verbose else False
 
-    try:
         start = time.time()
-
-        # make the response document
-        if args.xml:
-            root = minidom.Document()
-            environment = root.createElement('Environment')
-            environment.setAttribute('name', args.environment)
-            environment.setAttribute('date', time.asctime())
-            root.appendChild(environment)
 
         pretty = '&pretty=true' if args.pretty else ''
         opendap = '&has_opendap_url=true'
@@ -70,8 +56,6 @@ def main():
             with open(save_path_file, "w") as f:
                 f.write(xml_str)
 
-    except cmr.CMRException as e:
-        print(e)
 
 
 if __name__ == "__main__":
