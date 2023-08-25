4cdfd6ad898871be1fb7c8322f6f70494ce64d81
Samuel Lloyd <slloyd@opendap.org>
2023-08-25 13:30:47 -0600

    minor changes

--- bash-scripts/run_tests.sh
+++ bash-scripts/run_tests.sh
@@ -4,5 +4,5 @@ export PATH=/home/centos/pydmr/:$PATH
 
 #Run the tests and move them into a folder generated for today's date
 python3 opendap_providers.py -e PROD -T
-#python3 file_mover.py
+python3 file_mover.py
 
